c1a2796104ac745adf4603b7b35bdc51fddbe0f4
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2023-12-13 16:14:41 +0530

    i wont stop until i learn

---
+++ fourth.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+DATE=$(date)
+
+echo "Todays date and time is $DATE "
--- third.sh
+++ third.sh
@@ -4,4 +4,6 @@ x=$1
 y=$2
 z=$3
 
-echo "The sum of x&y&z = $x+$y+$z"
+k=$($x+$y+$z)
+
+echo "The sum of x&y&z = $k"
