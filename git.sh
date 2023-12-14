3dda1b143373c47c38d188126e23004c9fbad7fe
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2023-12-14 11:57:03 +0530

    precticing git

--- git.sh
+++ git.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 ID=$(id -u)
+
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -8,6 +9,7 @@ N="\e[0m"
 if [$ID -ne 0 ]
 then
 echo "$R You are not a root user,please try again with root access"
+exit 1
 else
 echo "please proceed ...."
 fi
@@ -15,9 +17,10 @@ fi
 VALIDATE() {
     if [ $1 -ne 0 ]
     then
-    echo -e"$R ERROR $N :: $2 .... $R FAILED"
+    echo -e "$R ERROR $N :: $2 .... $R FAILED"
+    exit 1
     else
-    echo -e"$2 .... $G SUCCESS"
+    echo -e "$2 .... $G SUCCESS"
     fi
 }
 
