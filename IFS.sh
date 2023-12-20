3d563e2a1387dd1aea73e7a2a1c94bdd00a01c90
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2023-12-20 12:52:41 +0530

    practicing

---
+++ IFS.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+file=/home/centos/practice/passport
+while IFS= read -r username rollnumber idnumber marks
+do
+echo "username: $username"
+echo "rollnumber: $rollnumber"
+echo "idnumber: $idnumber"
+echo "marks: $marks"
+done
+
+
