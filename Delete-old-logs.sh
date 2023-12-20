5591d6c47f53adf071443d8d37f8a8d856df8a62
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2023-12-20 12:06:45 +0530

    practicing

--- Delete-old-logs.sh
+++ Delete-old-logs.sh
@@ -7,11 +7,11 @@ fi
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
-$FILES_TO_DELETE >> while IFS= read -r line
+  while IFS= read -r line
 do
 echo "deleted file:$line"
 rm -rf $line
-done 
+done <<<$FILES_TO_DELETE
 
 
 
